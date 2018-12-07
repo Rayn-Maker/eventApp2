389e2477521bd5f5ffa99c8efd9b212920cd6c47
Radiance <okuzor15@tamu.edu>
2018-12-07 00:25:37 -0600

    Initial Commit

---
+++ eventApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eventApp2
+//
+//  Created by Radiance Okuzor on 12/7/18.
+//  Copyright © 2018 RayCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
