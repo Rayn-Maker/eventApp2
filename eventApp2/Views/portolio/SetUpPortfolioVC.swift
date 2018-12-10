7d77765637b06dbd26e8c2f8277da2fbf6382d66
Radiance <okuzor15@tamu.edu>
2018-12-09 20:41:23 -0600

    implementation of portfolio under const.

---
+++ eventApp2/Views/portolio/SetUpPortfolioVC.swift
@@ -0,0 +1,22 @@
+//
+//  SetUpPortfolioVC.swift
+//  eventApp2
+//
+//  Created by Radiance Okuzor on 12/9/18.
+//  Copyright © 2018 RayCo. All rights reserved.
+//
+
+import UIKit
+
+class SetUpPortfolioVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    // services offered are event planners caterers (business or individuals) venue provider (business) 
+
+}
